c63329cc41cde0d6338fe25f1a3c5ab7353b6e96
thushan-123 <thushanmadusanka456@gmail.com>
2025-09-18 22:41:05 +0530

    get user Input

--- main.cpp
+++ main.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 
+
+void shell () {
+
+    string input_line;
+
+    cout << "$ >";
+    getline(cin, input_line);
+    cout << endl;
+    
+}
+
+
+
 int main() {
 
-
-    return 0;
+    shell();
     
 }
