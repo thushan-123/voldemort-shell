22223928546eae4a0109fb9c30c6b5edd0ffb796
thushan-123 <thushanmadusanka456@gmail.com>
2025-09-22 21:04:31 +0530

    create function slpit space

--- main.cpp
+++ main.cpp
@@ -1,21 +1,33 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
+#include <sstream>
 using namespace std;
 
 vector<string> CMDS_ARRAY = {"type" ,"echo" ,"exit"};
+
+vector<string> split_by_space(const string& input){
+    vector<string> result;
+    istringstream iss(input);
+    string word;
+    while (iss >> word){
+        result.push_back(word);
+    }
+
+    return result;
+}
 
 string echo_cmd(string input){
     if(input.find("echo")){
         return  input.substr(5);
     }
+    return "";
 }
 
 string type_cmd(string input){
     string command_name = input.substr(5);
-    command_name.erase(remove(command_name.begin(), command_name.end(), ' '), command_name.end());
-    for (string& cmd : CMDS_ARRAY){
+    //command_name.erase(remove(command_name.begin(), command_name.end(), ' '), command_name.end());
+    for (const string& cmd : CMDS_ARRAY){
         if(command_name == cmd){
             return  cmd + "is a shell builtin";
         }
@@ -29,14 +41,16 @@ string invalied_cmd (string input){
 }
 
 string handle_commands(string input){
+    vector<string> cmd = split_by_space(input);
+    
     if (input.find("echo")){
         return echo_cmd(input);
     }else if(input.find("type")) {
         return type_cmd(input);
     }else{
-        invalied_cmd(input);
+        return invalied_cmd(input);
     }
-}
+};
 
 string output(string str){
 
