9eeafb1677f00b0310532a6f1fc5f3d0f208e862
thushan-123 <thushanmadusanka456@gmail.com>
2025-09-24 18:28:49 +0530

    move to command fn from main to command

---
+++ command/command.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <filesystem>
+#include <fstream>
+#include <bits/stdc++.h>
+
+using namespace std;
+namespace fs = std::filesystem;
+
+
+vector<string> CMDS_ARRAY = {"type" ,"echo" ,"exit"};
+
+vector<string> split_by_space(const string& input){
+    vector<string> result;
+    istringstream iss(input);
+    string word;
+    while (iss >> word){
+        result.push_back(word);
+    }
+
+    return result;
+}
+
+vector<string> split_string(const string& str, char delimiter) {
+    vector<string> tokens;
+    std::string currentToken;
+    for (char c : str) {
+        if (c == delimiter) {
+            tokens.push_back(currentToken);
+            currentToken = "";
+        } else {
+            currentToken += c;
+        }
+    }
+    tokens.push_back(currentToken); 
+    return tokens;
+}
+
+// filesystem::path
+
+string ls_cmd(){
+    // identify the path
+    return " ";
+
+}
+
+bool is_exists_exec(fs::path file_path){
+
+    if(!fs::exists(file_path)){
+        return false;
+    }
+
+    fs::file_status file_status = fs::status(file_path);
+    if (fs::is_regular_file(file_status) && (file_status.permissions() & fs::perms::owner_exec) != fs::perms::none ){
+            return true;
+    }
+
+    return false;
+    
+
+}
+
+string echo_cmd(string input){
+    vector<string> inputs = split_by_space(input);
+    if(inputs[0] == "echo"){
+        if (inputs.size() >1){
+            return  input.substr(5);
+        }else {
+            return " ";
+        }
+        
+    }
+    return " ";
+}
+
+string type_cmd(string input, char *path_ptr){
+    vector<string> inputs = split_by_space(input);
+    string command_name = input.substr(5);
+
+    char *path_ptr1 = path_ptr;
+    string path_str(path_ptr1);
+
+    vector<string> paths = split_string(path_str, ':');
+    
+    
+    for (auto current_path: paths){
+        fs::path file_path = fs::path(current_path)/inputs[1];
+
+        if(is_exists_exec(file_path) && inputs[1] != "echo"){
+            return inputs[1] + " is " + file_path.string();
+        }
+    }      
+
+    for (const string& cmd : CMDS_ARRAY){
+        if(inputs[1] == cmd){
+            return  cmd + " is a shell builtin";
+        }
+    }
+        
+    
+    return "invalid_command: not found";
+}
+
+string invalied_cmd (string input){
+    string invalied_cmd = "invalied_command: ";
+    return invalied_cmd + input;
+}
